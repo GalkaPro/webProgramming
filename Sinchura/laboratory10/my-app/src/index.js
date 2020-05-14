593d47624350ac6bb364144535862015896f0f1a
Sinchura Mary <mariasincura@gmail.com>
2020-05-14 13:49:08 +0300

    lab9

---
+++ Sinchura/laboratory10/my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import './index.css';
+
+
+ReactDOM.render( < div >
+    <
+    h1 > Sinchura Mariia < /h1> <p > This is a paragraph about me... </p >
+    <
+    ul >
+    <
+    li > America < /li> <li > Japan </li > < li > Czech Republic < /li> </ul > < /div>,
+    document.getElementById("root")
+)
